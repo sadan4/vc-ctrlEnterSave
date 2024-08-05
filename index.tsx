e53685c7b199172947f75f6467e4005869fe53a9
sadan <117494111+sadan4@users.noreply.github.com>
2024-08-05 19:25:20 -0400

    create plugin CtrlEnterSave

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "CtrlEnterSave",
+    description: "",
+    authors: [
+        {
+            name: "sadan",
+            id: 521819891141967883n
+        }
+    ]
+})
