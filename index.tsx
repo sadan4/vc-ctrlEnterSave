024404d2a076339e05168495c957d0ddc1a9f713
sadan <117494111+sadan4@users.noreply.github.com>
2024-08-05 23:43:12 -0400

    create plugin

--- index.tsx
+++ index.tsx
@@ -1,4 +1,6 @@
 import definePlugin from "@utils/types";
+import { useEffect } from "@webpack/common";
+
 
 export default definePlugin({
     name: "CtrlEnterSave",
@@ -8,5 +10,27 @@ export default definePlugin({
             name: "sadan",
             id: 521819891141967883n
         }
-    ]
+    ],
+    patches: [
+        {
+            find: ".SETTINGS_NOTICE_MESSAGE",
+            replacement: {
+                match: /onSave:(\i).*?children:\[/,
+                replace: "$&$self.KeyListener($1),"
+            }
+        }
+    ],
+
+    KeyListener(onClickFunc: () => void){
+        useEffect(() => {
+            function eventListener(ev: KeyboardEvent){
+                if(ev.ctrlKey && ev.key === "Enter") onClickFunc();
+            }
+            document.addEventListener("keydown", eventListener);
+            return () => {
+                document.removeEventListener("keydown", eventListener)
+            }
+        });
+        return <></>;
+    }
 })
