92af585673356c3d6eb27e31c2d122ddead85b1d
sadan <117494111+sadan4@users.noreply.github.com>
2024-08-13 01:09:56 -0400

    guh

--- index.tsx
+++ index.tsx
@@ -39,7 +39,7 @@ function KeybindRecorder({ onBind }: {
                 alt: ev.altKey,
                 ctrl: ev.ctrlKey,
                 shift: ev.shiftKey,
-                meta: ev.which === 224,
+                meta: ev.metaKey,
                 key: ev.key
             });
         }
@@ -58,7 +58,7 @@ function KeymapElement(props: IPluginOptionComponentProps) {
     if (keybind.alt) keybindString.push("alt");
     if (keybind.ctrl) keybindString.push("mod");
     if (keybind.shift) keybindString.push("shift");
-    if (keybind.meta) keybindString.push("mod+cmd");
+    if (keybind.meta) keybindString.push("cmd");
     if (keybind.key) keybindString.push(keybind.key.toLowerCase());
     // dont let them save if they're recording
     return <>
